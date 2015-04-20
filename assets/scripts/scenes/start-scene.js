38d0ba7a3ad117c0f01b8372cc93b9db7642cce2
Jake Harris <milieu@javakat.io>
2015-04-20 14:25:19 -0500

    Update title screen header

--- assets/scripts/scenes/start-scene.js
+++ assets/scripts/scenes/start-scene.js
@@ -2,7 +2,7 @@ function StartScene() {
 
   var name = 'Start'
   var DEFAULT_ENTITIES = [
-      new Sprite({ img: 'assets/sprites/title.png', x: 50, y: 50, width: vpwidth() - 100, height: 400 }),
+      new Sprite({ img: 'assets/sprites/title.png', x: 0, y: 50, width: vpwidth() - 100, height: 400 }),
       new Text({ type: 'MenuItem', text: 'Press any key to start', y: height/2 + 44*2 }),
       new Text({ type: 'MenuItem', text: 'Player 1: WASD to move, Q and E to insult', y: height/2 + 44*4}),
       new Text({ type: 'MenuItem', text: 'Player 2: Arrow keys, / and Shift to insult', y: height/2 + 44*5})
