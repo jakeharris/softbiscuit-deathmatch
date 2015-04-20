687baf03b4b9af4dba3d9ad7f2e15a440103916a
Jake Harris <milieu@javakat.io>
2015-04-20 14:25:37 -0500

    Slightly refactor dunk scene entity code

--- assets/scripts/scenes/dunk-scene.js
+++ assets/scripts/scenes/dunk-scene.js
@@ -2,44 +2,45 @@ function DunkScene() {
 
   var name = 'Dunk',
       DEFAULT_ENTITIES = [
-        new Text     ({ type: 'Title', text: 'DUNKING SCENE' }),
         new Sprite   ({ img: 'assets/sprites/table.png', x: 25, y: vpheight() - 200, width: vpwidth() - 50, height: 100}),
         new Player   ({ player: PlayerNumber.ONE, x: 100, y: 100 }),
         new Player   ({ player: PlayerNumber.TWO, x: vpwidth() - 300, y: 100 }),
         new TimerBar ({ x: vpwidth() / 2 - 50, y: vpheight() * 3 / 4 })
       ],
       handleEvent = function (e) {
-        var p1d = this.entities[2].direction,
-            p2d = this.entities[3].direction,
+        var player1 = 1,
+            player2 = 2,
+            p1d = this.entities[player1].direction,
+            p2d = this.entities[player2].direction,
             key = e.which
 
         if      (key == '65')  p1d = Direction.LEFT
         else if (key == '87')  p1d = Direction.UP
         else if (key == '68')  p1d = Direction.RIGHT
         else if (key == '83')  p1d = Direction.DOWN
-        else if (key == '81' && !paused && this.entities[2].canInsult(Hindrances.REVERSE)) {
+        else if (key == '81' && !paused && this.entities[player1].canInsult(Hindrances.REVERSE)) {
           this.entities.push(new Hindrance(
             {
               player: PlayerNumber.TWO,
               type: Hindrances.REVERSE
             }
           ))
-          this.entities[2].hasUsedReverse = true
-          this.entities[3].isBeingReversed = true
+          this.entities[player1].hasUsedReverse = true
+          this.entities[player2].isBeingReversed = true
         }
         else if (key == '37') p2d = Direction.LEFT
         else if (key == '38') p2d = Direction.UP
         else if (key == '39') p2d = Direction.RIGHT
         else if (key == '40') p2d = Direction.DOWN
-        else if (key == '191' && this.entities[3].canInsult(Hindrances.REVERSE)) {
+        else if (key == '191' && this.entities[player2].canInsult(Hindrances.REVERSE)) {
           this.entities.push(new Hindrance(
             {
               player: PlayerNumber.ONE,
               type: Hindrances.REVERSE
             }
           ))
-          this.entities[3].hasUsedReverse = true
-          this.entities[2].isBeingReversed = true
+          this.entities[player2].hasUsedReverse = true
+          this.entities[player1].isBeingReversed = true
         }
         else if (key == '27' || key == '80') pause()
         else if (key == '81' && paused) {
@@ -70,12 +71,11 @@ function DunkScene() {
           }
         }
 
-        this.entities[2].direction = p1d
-        this.entities[3].direction = p2d
+        this.entities[player1].direction = p1d
+        this.entities[player2].direction = p2d
 
         inputs.push(p1d)
-      }.bind(this),
-      insultCooldown = 3000
+      }.bind(this)
 
   Scene.call(this, name, DEFAULT_ENTITIES, handleEvent)
 
@@ -83,7 +83,7 @@ function DunkScene() {
     console.log(name + ' scene is starting...')
     document.addEventListener('keydown', this.handleEvent)
     this.initialized = true
-    DEFAULT_ENTITIES[4].reset()
+    DEFAULT_ENTITIES[3].reset()
     return this.test()
   };
 
@@ -91,19 +91,19 @@ function DunkScene() {
     if(!this.initialized) this.init();
     if(!this.entities) this.entities = cloneArray(DEFAULT_ENTITIES)
 
-    if(this.entities[4].percentageComplete >= 100) this.end()
+    if(this.entities[3].percentageComplete >= 100) this.end()
 
-    if(this.entities.length > 5) {
+    if(this.entities.length > 4) {
       for(var ent in this.entities) {
         var en = this.entities[ent]
         if(en instanceof Hindrance && !en.alive) {
           if (en.player == PlayerNumber.ONE) {
-            if     (en.type == Hindrances.REVERSE) this.entities[2].isBeingReversed = false
-            else if(en.type == Hindrances.JITTERS) this.entities[2].isBeingJittered = false
+            if     (en.type == Hindrances.REVERSE) this.entities[1].isBeingReversed = false
+            else if(en.type == Hindrances.JITTERS) this.entities[1].isBeingJittered = false
           }
           else if(en.player == PlayerNumber.TWO) {
-            if     (en.type == Hindrances.REVERSE) this.entities[3].isBeingReversed = false
-            else if(en.type == Hindrances.JITTERS) this.entities[3].isBeingJittered = false
+            if     (en.type == Hindrances.REVERSE) this.entities[2].isBeingReversed = false
+            else if(en.type == Hindrances.JITTERS) this.entities[2].isBeingJittered = false
           }
           this.entities.splice(ent, 1)
         }
