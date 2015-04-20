a4e3523c2523b4c904703716eec39b488572c73f
Jake Harris <milieu@javakat.io>
2015-04-19 22:46:16 -0500

    Clean up dunk scene code

--- assets/scripts/scenes/dunk-scene.js
+++ assets/scripts/scenes/dunk-scene.js
@@ -1,40 +1,41 @@
 function DunkScene() {
 
-  var name = 'Dunk'
-  var DEFAULT_ENTITIES = [
-      new Text     ({ type: 'Title', text: 'DUNKING SCENE' }),
-      new Sprite   ({ img: 'assets/sprites/table.png', x: 25, y: vpheight() - 200, width: vpwidth() - 50, height: 100}),
-      new Player   ({ player: PlayerNumber.ONE, x: 100, y: 100 }),
-      new Player   ({ player: PlayerNumber.TWO, x: vpwidth() - 300, y: 100, cookieSprite: 'assets/sprites/biscuit-bishop.png' }),
-      new TimerBar ({ x: vpwidth() / 2 - 50, y: vpheight() * 3 / 4 })
-  ]
-  var handleEvent = function (e) {
-    var p1d = scenes[Scenes.DUNK].entities[2].direction,
-        p2d = scenes[Scenes.DUNK].entities[3].direction,
-        key = e.which
+  var name = 'Dunk',
+      DEFAULT_ENTITIES = [
+        new Text     ({ type: 'Title', text: 'DUNKING SCENE' }),
+        new Sprite   ({ img: 'assets/sprites/table.png', x: 25, y: vpheight() - 200, width: vpwidth() - 50, height: 100}),
+        new Player   ({ player: PlayerNumber.ONE, x: 100, y: 100 }),
+        new Player   ({ player: PlayerNumber.TWO, x: vpwidth() - 300, y: 100, cookieSprite: 'assets/sprites/biscuit-bishop.png' }),
+        new TimerBar ({ x: vpwidth() / 2 - 50, y: vpheight() * 3 / 4 })
+      ],
+      handleEvent = function (e) {
+        var p1d = this.entities[2].direction,
+            p2d = this.entities[3].direction,
+            key = e.which
 
-    if      (key == '65')  p1d = Direction.LEFT
-    else if (key == '87')  p1d = Direction.UP
-    else if (key == '68')  p1d = Direction.RIGHT
-    else if (key == '83')  p1d = Direction.DOWN
+        if      (key == '65')  p1d = Direction.LEFT
+        else if (key == '87')  p1d = Direction.UP
+        else if (key == '68')  p1d = Direction.RIGHT
+        else if (key == '83')  p1d = Direction.DOWN
 
-    else if (key == '37') p2d = Direction.LEFT
-    else if (key == '38') p2d = Direction.UP
-    else if (key == '39') p2d = Direction.RIGHT
-    else if (key == '40') p2d = Direction.DOWN
+        else if (key == '37') p2d = Direction.LEFT
+        else if (key == '38') p2d = Direction.UP
+        else if (key == '39') p2d = Direction.RIGHT
+        else if (key == '40') p2d = Direction.DOWN
 
-    else if (key == '27' || key == '80') pause()
-    else if (key == '81' && paused) {
-      scenes[Scenes.DUNK].end()
-      pause()
-      currentScene = Scenes.START
-    }
+        else if (key == '27' || key == '80') pause()
+        else if (key == '81' && paused) {
+          this.end()
+          pause()
+          currentScene = Scenes.START
+        }
 
-    scenes[Scenes.DUNK].entities[2].direction = p1d
-    scenes[Scenes.DUNK].entities[3].direction = p2d
+        this.entities[2].direction = p1d
+        this.entities[3].direction = p2d
 
-    inputs.push(p1d)
-  }.bind(this)
+        inputs.push(p1d)
+      }.bind(this),
+      insultCooldown = 3000
 
   Scene.call(this, name, DEFAULT_ENTITIES, handleEvent)
 
