7b75372cc2078e0c2805796835c975e59b09928f
Jake Harris <milieu@javakat.io>
2015-04-20 12:24:36 -0500

    Add stripey background

--- assets/scripts/scenes/dunk-scene.js
+++ assets/scripts/scenes/dunk-scene.js
@@ -113,6 +113,37 @@ function DunkScene() {
     return this.move()
   };
 
+  this.render = function () {
+    var lightPink = 'rgb(247,192,218)',
+        lightPinkWidth = 45,
+        darkPink  = 'rgb(238,168,203)',
+        darkPinkWidth = 20,
+        x = 0
+
+    for(var i = 0; i <= vpwidth() / darkPinkWidth; i++) {
+      ctx.beginPath()
+
+      var width = 0
+      if(i % 2 === 0) {
+        ctx.fillStyle = lightPink
+        width = lightPinkWidth
+      }
+      else {
+        ctx.fillStyle = darkPink
+        width = darkPinkWidth
+      }
+      ctx.fillRect(x, 0, width, vpheight())
+      x += width
+
+      ctx.closePath()
+    }
+
+    if(!this.entities) return;
+    this.entities.forEach(function (e, i, a) {
+      e.render()
+    });
+  }
+
   this.end = function () {
     this.initialized = false
     console.log(name + ' scene is ending...')
