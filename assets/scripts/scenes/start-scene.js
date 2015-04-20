0d59bc57d60de44fea50f11ec7dd7e7c74b488dc
Jake Harris <milieu@javakat.io>
2015-04-20 17:09:15 -0500

    Make inputs more clear

--- assets/scripts/scenes/start-scene.js
+++ assets/scripts/scenes/start-scene.js
@@ -5,11 +5,11 @@ function StartScene() {
       new Sprite({ img: 'assets/sprites/title.png', x: 0, y: 50, width: vpwidth() - 100, height: 400 }),
       new Text({ type: 'MenuItem', text: 'Press any key to start', y: height/2 + 44*2 }),
       new Text({ type: 'MenuItem', text: 'Player 1: WASD to move, Q and E to insult', y: height/2 + 44*4}),
-      new Text({ type: 'MenuItem', text: 'Player 2: Arrow keys, / and Shift to insult', y: height/2 + 44*5})
+      new Text({ type: 'MenuItem', text: 'Player 2: Arrow keys, </> and <Shift> to insult', y: height/2 + 44*5})
   ]
   var handleEvent = function (e) {
     scenes[Scenes.START].initialized = false
-    currentScene = 1
+    currentScene = Scenes.DUNK
     document.removeEventListener('keydown', this.handleEvent)
   }.bind(this)
 
