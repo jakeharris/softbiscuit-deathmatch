4f2d77a0c8e9a4abafbdfbb7cf1ed37d466540ab
Jake Harris <milieu@javakat.io>
2015-04-20 11:14:26 -0500

    Add reversed basic graphics

--- assets/scripts/scenes/dunk-scene.js
+++ assets/scripts/scenes/dunk-scene.js
@@ -5,7 +5,7 @@ function DunkScene() {
         new Text     ({ type: 'Title', text: 'DUNKING SCENE' }),
         new Sprite   ({ img: 'assets/sprites/table.png', x: 25, y: vpheight() - 200, width: vpwidth() - 50, height: 100}),
         new Player   ({ player: PlayerNumber.ONE, x: 100, y: 100 }),
-        new Player   ({ player: PlayerNumber.TWO, x: vpwidth() - 300, y: 100, cookieSprite: 'assets/sprites/biscuit-bishop.png' }),
+        new Player   ({ player: PlayerNumber.TWO, x: vpwidth() - 300, y: 100 }),
         new TimerBar ({ x: vpwidth() / 2 - 50, y: vpheight() * 3 / 4 })
       ],
       handleEvent = function (e) {
@@ -17,26 +17,30 @@ function DunkScene() {
         else if (key == '87')  p1d = Direction.UP
         else if (key == '68')  p1d = Direction.RIGHT
         else if (key == '83')  p1d = Direction.DOWN
-        else if (key == '81' && !paused && this.entities[2].canInsult(Hindrances.REVERSE))
+        else if (key == '81' && !paused && this.entities[2].canInsult(Hindrances.REVERSE)) {
           this.entities.push(new Hindrance(
             {
               player: PlayerNumber.TWO,
               type: Hindrances.REVERSE
             }
           ))
-
+          this.entities[2].hasUsedReverse = true
+          this.entities[3].isBeingReversed = true
+        }
         else if (key == '37') p2d = Direction.LEFT
         else if (key == '38') p2d = Direction.UP
         else if (key == '39') p2d = Direction.RIGHT
         else if (key == '40') p2d = Direction.DOWN
-        else if (key == '191' && this.entities[3].canInsult(Hindrances.REVERSE))
+        else if (key == '191' && this.entities[3].canInsult(Hindrances.REVERSE)) {
           this.entities.push(new Hindrance(
             {
               player: PlayerNumber.ONE,
               type: Hindrances.REVERSE
             }
           ))
-
+          this.entities[3].hasUsedReverse = true
+          this.entities[2].isBeingReversed = true
+        }
         else if (key == '27' || key == '80') pause()
         else if (key == '81' && paused) {
           this.end()
@@ -76,27 +80,44 @@ function DunkScene() {
   Scene.call(this, name, DEFAULT_ENTITIES, handleEvent)
 
   this.init = function () {
-    console.log(name + ' scene is starting...');
-    document.addEventListener('keydown', this.handleEvent);
-    this.initialized = true;
-    DEFAULT_ENTITIES[4].reset();
-    return this.test();
+    console.log(name + ' scene is starting...')
+    document.addEventListener('keydown', this.handleEvent)
+    this.initialized = true
+    DEFAULT_ENTITIES[4].reset()
+    return this.test()
   };
 
   this.logic = function () {
     if(!this.initialized) this.init();
-    if(!this.entities) this.entities = cloneArray(DEFAULT_ENTITIES);
+    if(!this.entities) this.entities = cloneArray(DEFAULT_ENTITIES)
 
-    if(this.entities[4].percentageComplete >= 100) this.end();
+    if(this.entities[4].percentageComplete >= 100) this.end()
 
-    return this.move();
+    if(this.entities.length > 5) {
+      for(var ent in this.entities) {
+        var en = this.entities[ent]
+        if(en instanceof Hindrance && !en.alive) {
+          if (en.player == PlayerNumber.ONE) {
+            if     (en.type == Hindrances.REVERSE) this.entities[2].isBeingReversed = false
+            else if(en.type == Hindrances.JITTERS) this.entities[2].isBeingJittered = false
+          }
+          else if(en.player == PlayerNumber.TWO) {
+            if     (en.type == Hindrances.REVERSE) this.entities[3].isBeingReversed = false
+            else if(en.type == Hindrances.JITTERS) this.entities[3].isBeingJittered = false
+          }
+          this.entities.splice(ent, 1)
+        }
+      }
+    }
+
+    return this.move()
   };
 
   this.end = function () {
-    this.initialized = false;
-    console.log(name + ' scene is ending...');
-    document.removeEventListener('keydown', this.handleEvent);
+    this.initialized = false
+    console.log(name + ' scene is ending...')
+    document.removeEventListener('keydown', this.handleEvent)
     currentScene = Scenes.CONSUME
-    this.entities = cloneArray(DEFAULT_ENTITIES);
+    this.entities = cloneArray(DEFAULT_ENTITIES)
   };
 }
