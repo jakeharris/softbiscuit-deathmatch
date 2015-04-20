f27339fbe827b8182d56dc6a32b4b158deffc9d7
Jake Harris <milieu@javakat.io>
2015-04-19 22:46:30 -0500

    Pretend insults exist

--- assets/scripts/scenes/start-scene.js
+++ assets/scripts/scenes/start-scene.js
@@ -4,8 +4,8 @@ function StartScene() {
   var DEFAULT_ENTITIES = [
       new Sprite({ img: 'assets/sprites/title.png', x: 50, y: 50, width: vpwidth() - 100, height: 400 }),
       new Text({ type: 'MenuItem', text: 'Press any key to start', y: height/2 + 44*2 }),
-      new Text({ type: 'MenuItem', text: 'Player 1: WASD', y: height/2 + 44*4}),
-      new Text({ type: 'MenuItem', text: 'Player 2: Arrow keys', y: height/2 + 44*5})
+      new Text({ type: 'MenuItem', text: 'Player 1: WASD to move, Q and E to insult', y: height/2 + 44*4}),
+      new Text({ type: 'MenuItem', text: 'Player 2: Arrow keys, / and Shift to insult', y: height/2 + 44*5})
   ]
   var handleEvent = function (e) {
     scenes[Scenes.START].initialized = false
